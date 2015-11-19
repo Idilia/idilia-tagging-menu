bf8fa3f60732cc14e75c007b540f8089e1301827
Pierre Belzile <pierre.belzile@idilia.com>
2015-11-19 16:43:07 -0500

    ensure open-sans font is available

--- src/jquery.sense_card.js
+++ src/jquery.sense_card.js
@@ -202,4 +202,20 @@ if (typeof Object.create !== "function") {
     deleted: null,
     lgcc: 'https://lgcc.idilia.com/lgcc/'
   };
+  
+  /**
+   * Load the open-sans font to ensure that available
+   */
+  WebFontConfig = {
+    google: { families: [ 'Open+Sans::latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })();
+  
 }(jQuery, window, document));
